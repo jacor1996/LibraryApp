46731e85ff1a25d37f141583c4bad12feed4fb87
Jacek Noga <jaceknoga11@gmail.com>
2018-10-02 17:04:34 +0200

    Added data annotations.

--- Library.Entities/Book.cs
+++ Library.Entities/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,12 @@ namespace Library.Entities
 {
     public class Book : DataObject
     {
+        [Required(ErrorMessage = "Title is required.")]
+        [StringLength(256)]
         public string Title { get; set; }
+
+        [Required(ErrorMessage = "Category is required.")]
+        [StringLength(256)]
         public string Category { get; set; }
 
         public ICollection<Author> Authors { get; set; }
